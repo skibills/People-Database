c25ea1dcf15e1952fe0d913f0a10c4b68f7fc59a
skibills <77559071+skibills@users.noreply.github.com>
2021-01-21 23:53:30 -0500

    Update name_search.py

--- name_search.py
+++ name_search.py
@@ -1,27 +1,37 @@
 
 def start():
     from names_list import people_database
+    loopRun = True
 
-    Name = input('Please enter a name: ').lower()
-    print('You typed in the name ' + Name.capitalize())
-    try:
-        Persons_data = people_database[Name]
-        print('Name: ' + Name.capitalize())
-        print('Sex: ' + Persons_data[0])
-        print('Weight: ' + Persons_data[1])
-        more()
-    except:
-        print('That name was not found in the database.')
-        more()
-def more():
-    More = input('Would you like to search for another name? ')
-    if More == 'Yes':
-        start()
-    if More == 'No':
-        print('Goodbye!')
-        quit()
-    else:
-        print('Please enter Yes or No.')
-        more()
+    
+    while loopRun == True:
+        Name = input('Please enter a name: ').lower()
+        print('You typed in the name ' + Name.capitalize())
+        try:
+            Persons_data = people_database[Name]
+            print('Name: ' + Name.capitalize())
+            print('Sex: ' + Persons_data[0])
+            print('Weight: ' + Persons_data[1])
+
+        except:
+            print('That name was not found in the database.')
+
+        isNotValid = True       #primes loop - line 19
+        while isNotValid == True:
+            userInput = input('Would you like to search for another name? ').capitalize()
+            
+            if userInput == 'Yes':
+                #loopRun = True
+                isNotValid = False
+
+            elif userInput == 'No':
+                loopRun = False
+                isNotValid = False
+
+            else:
+                print('Please enter Yes or No.')
+                isNotValid = True
+
+               
+    print("No, Goodbye!")
 start()
-
